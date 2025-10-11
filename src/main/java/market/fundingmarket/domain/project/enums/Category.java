793d2f74fc853ce65434f73decc0aa9fa51a9f85
mj-song00 <niveus128@gmail.com>
2025-10-11 14:07:10 +0900

    feat: project, creator profile enum 설정

---
+++ src/main/java/market/fundingmarket/domain/project/enums/Category.java
@@ -0,0 +1,6 @@
+package market.fundingmarket.domain.project.enums;
+
+public enum Category {
+   GAME, HOME, LIVING, ART, PET, WEBTOON, GOODS, CLOTHES, TECH,
+    PERFUME, MUSIC, BAG, BEAUTY
+}
---
+++ src/main/java/market/fundingmarket/domain/project/enums/FundingStatus.java
@@ -0,0 +1,5 @@
+package market.fundingmarket.domain.project.enums;
+
+public enum FundingStatus {
+    InPROGRESS, INTERRUPTION, COMPLETION
+}
