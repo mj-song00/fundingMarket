6cc57087b55ee3b0c6983c032ed8a3f7a35476d7
mj-song00 <niveus128@gmail.com>
2025-10-17 16:40:51 +0900

    feat: auth user argument resolvers 등록

---
+++ src/main/java/market/fundingmarket/common/config/AdminAtrgumentResolver.java
@@ -0,0 +1,39 @@
+package market.fundingmarket.common.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import market.fundingmarket.common.annotation.Auth;
+import market.fundingmarket.domain.user.dto.AuthUser;
+import market.fundingmarket.domain.user.enums.UserRole;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.UUID;
+
+public class AdminAtrgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(Auth.class) != null;
+    }
+
+    // AuthUser 객체를 생성하여 반환
+    @Override
+    public Object resolveArgument(
+            @Nullable MethodParameter parameter,
+            @Nullable ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            @Nullable WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        // JwtFilter 에서 set 한 userId, username, userRole 값을 가져옴
+        UUID id = (UUID) request.getAttribute("id");
+        String username = (String) request.getAttribute("username");
+        String roleStr = (String) request.getAttribute("role");
+        UserRole role = UserRole.valueOf(roleStr);
+        return new AuthUser(id, username, role);
+    }
+}
---
+++ src/main/java/market/fundingmarket/common/config/WebConfig.java
@@ -0,0 +1,29 @@
+package market.fundingmarket.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    // ArgumentResolver 등록
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키 허용
+    }
+}
