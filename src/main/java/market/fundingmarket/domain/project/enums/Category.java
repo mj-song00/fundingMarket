adc146e23aa90f55f2a4d4a50d0b8bafa3703f45
mj-song00 <niveus128@gmail.com>
2025-10-26 21:58:32 +0900

    chore: enum key, value 추가

--- src/main/java/market/fundingmarket/domain/project/enums/Category.java
+++ src/main/java/market/fundingmarket/domain/project/enums/Category.java
@@ -1,6 +1,27 @@
 package market.fundingmarket.domain.project.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-   GAME, HOME, LIVING, ART, PET, WEBTOON, GOODS, CLOTHES, TECH,
-    PERFUME, MUSIC, BAG, BEAUTY
+   GAME("게임"),
+    HOME("홈"),
+    LIVING("리빙"),
+    ART("아트"),
+    PET("애완동물"),
+    WEBTOON("웹툰"),
+    GOODS("굿즈"),
+    CLOTHES("의류"),
+    TECH("테크"),
+    PERFUME("향수"),
+    MUSIC("음반"),
+    BAG("가방"),
+    BEAUTY("미용");
+
+
+    private final String message;
+
+    Category(String message) {
+        this.message = message;
+    }
 }
