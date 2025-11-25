d13c99cdafa293e96ec6eb7e70e908744e637f10
mj-song00 <niveus128@gmail.com>
2025-11-25 23:06:21 +0900

    chore: class to record

--- src/main/java/market/fundingmarket/domain/payment/dto/response/CanceledResponse.java
+++ src/main/java/market/fundingmarket/domain/payment/dto/response/CanceledResponse.java
@@ -1,8 +1,5 @@
 package market.fundingmarket.domain.payment.dto.response;
 
-import lombok.Getter;
-
-@Getter
 public record CanceledResponse(
         String paymentKey, String orderId, int amount, String status) {
 }
--- src/main/java/market/fundingmarket/domain/payment/dto/response/PaymentResponse.java
+++ src/main/java/market/fundingmarket/domain/payment/dto/response/PaymentResponse.java
@@ -1,7 +1,4 @@
 package market.fundingmarket.domain.payment.dto.response;
 
-import lombok.Getter;
-
-@Getter
 public record PaymentResponse(String paymentKey, String orderId, int amount) {
 }
