23aa3dedaef177e57a548642d853843d0c48803b
mj-song00 <niveus128@gmail.com>
2025-12-09 20:46:46 +0900

    chore: 카테고리 수정

--- src/main/java/market/fundingmarket/domain/project/enums/Category.java
+++ src/main/java/market/fundingmarket/domain/project/enums/Category.java
@@ -4,20 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum Category {
-   GAME("게임"),
+    GAME("게임"),
     HOME("홈"),
     LIVING("리빙"),
     ART("아트"),
     PET("애완동물"),
-    WEBTOON("웹툰"),
-    GOODS("굿즈"),
-    CLOTHES("의류"),
+    FOOD("음식"),
+    BOOK("도서"),
+    FASHION("의류"),
     TECH("테크"),
-    PERFUME("향수"),
+    BEAUTY("화장품"),
     MUSIC("음반"),
-    BAG("가방"),
-    BEAUTY("미용"),
-    ANIME("애니");
+    MOVIE("영화"),
+    TRAVEL("여행"),
+    CAR("차량");
 
 
     private final String message;
